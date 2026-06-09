3ca3a26f70413625e590fefad9ee0c409ed885cc
yves27 <55056265+yves27@users.noreply.github.com>
2026-06-09 22:29:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 // Ne le modifiez que si vous changez de projet Supabase.
 // ============================================================
 
-const DEFAULT_SUPA_URL = 'VOTRE_URL_SUPABASE_ICI';
-const DEFAULT_SUPA_KEY = 'VOTRE_CLÉ_SUPABASE_ICI';
+const DEFAULT_SUPA_URL = 'https://fsqvhlxlsanohadhoclo.supabase.co';
+const DEFAULT_SUPA_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzcXZobHhsc2Fub2hhZGhvY2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMTU4NjQsImV4cCI6MjA5NjU5MTg2NH0.HLOnS13liJ8EaPj2mtKP7Kv8BqafKu_c8S4Ffvqdv3k';
 
 // Exemple :
 // const DEFAULT_SUPA_URL = 'https://xxxxxxxxxxxx.supabase.co';
